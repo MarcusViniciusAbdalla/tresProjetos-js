88ff8f5da9383f989e1bf3aabeea4aaf2f192074
MarcusViniciusAbdalla <marcusviniciusabdalla@gmail.com>
2021-12-16 22:05:16 -0300

    Criando Três Projetos

---
+++ urna/etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo:"Vereador",
+        numeros:5,
+        candidatos: [
+            {
+                numero:'38111',
+                nome: 'Fulano de Tal',
+                partido: 'Abc',
+                fotos:[
+                    {url: "Design sem nome (18).png" , legenda: 'Vereador'}
+                ]
+            },
+            {
+                numero:'77222',
+                nome:'Beltrana de Tal',
+                partido: 'TOP',
+                fotos:[
+                    {url:'Design sem nome (19).png', legenda: 'Vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo:'PREFEITO',
+        numeros:2,
+        candidatos:[
+            {
+                numero:'99',
+                nome:'Ciclano',
+                partido:'ABC',
+                Vice:'Cic',
+                fotos:[
+                    {url:'99.png', legenda:'Prefeito'},
+                    {url:'99_2.png', legenda:'Vice-Prefeita', small:true}
+                ]
+            },
+            {
+                numero:'84',
+                nome:'Zulano',
+                Partido:"Mió",
+                Vice:"Zula",
+                fotos:[
+                    {url:'84.png',legenda:'Prefeita'},
+                    {url:'84_2.png', legenda:'Vice-Prefeita', small:true}
+                ]
+            },
+        ]
+    }
+];
