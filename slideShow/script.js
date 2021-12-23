5facaec5c62d4edbdd9654fea4403eeb783b8499
MarcusViniciusAbdalla <marcusviniciusabdalla@gmail.com>
2021-12-23 20:09:06 -0300

    Fazendo os Botões de rolagem de slide

--- slideShow/script.js
+++ slideShow/script.js
@@ -1,6 +1,15 @@
 let totalSildes = document.querySelectorAll('.slider--item').length;
+let currentSlide = 0;
 
 document.querySelector('.slider--widht').style.widht = `calc(100vw * ${totalSildes})`;
 
 document.querySelector('.slider--controls').style.height = `${document.querySelector('.slider').clientHeight}px`;
+
+function goPrev() {
+
+}
+
+function goNext() {
+
+}
 
