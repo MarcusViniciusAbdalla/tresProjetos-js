d5a5698dffa44aa5f2a4dbf86a02767202407350
MarcusViniciusAbdalla <marcusviniciusabdalla@gmail.com>
2021-12-23 20:20:14 -0300

    Update script.js

--- slideShow/script.js
+++ slideShow/script.js
@@ -6,10 +6,20 @@ document.querySelector('.slider--widht').style.widht = `calc(100vw * ${totalSild
 document.querySelector('.slider--controls').style.height = `${document.querySelector('.slider').clientHeight}px`;
 
 function goPrev() {
-
+    currentSlide--;
+    if (currentSlide < 0) {
+        currentSlide = totalSildes - 1;
+    }
+    updateMargin()
 }
 
 function goNext() {
-
+    currentSlide++;
+    if (currentSlide > (totalSildes - 1)) {
+        currentSlide = 0;
+    }
 }
 
+function updateMargin() {
+
+}
